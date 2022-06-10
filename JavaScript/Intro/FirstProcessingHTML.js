590f7fe488befc182acfed25bbc5cfb410d4f34f
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-06-10 10:46:27 +0530

    practicing queryselector with sample.html

--- JavaScript/Intro/FirstProcessingHTML.js
+++ JavaScript/Intro/FirstProcessingHTML.js
@@ -1,5 +1,5 @@
 // npm install jsdom
-// node FirstProcessingHTML.js --source=download.html
+// node FirstProcessingHTML.js --source=sample.html
 
 let minimist = require("minimist");
 let fs = require("fs");
@@ -14,5 +14,8 @@ fs.readFile(args.source, "utf-8", function(err, html){
     let dom = new jsdom.JSDOM(html);
     let document = dom.window.document;
 
-    console.log(document.title);
+    let elements = document.querySelectorAll(".b");
+    console.log(elements[0].textContent);
+    console.log(elements[1].textContent);
+    console.log(elements[2].textContent);
 })
