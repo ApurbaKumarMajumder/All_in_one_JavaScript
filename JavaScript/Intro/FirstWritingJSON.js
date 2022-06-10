fc21e84e7d21ba7c1a82506088d8ae7d3a2e13a0
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-06-10 17:57:24 +0530

    first working with JSON

--- JavaScript/Intro/FirstWritingJSON.js
+++ JavaScript/Intro/FirstWritingJSON.js
@@ -6,57 +6,51 @@ let minimist = require("minimist");
 let fs = require("fs");
 
 let args = minimist(process.argv);
-console.log(args.dest);
 
-// JSON = Javascript Object Notation
-// JSON means saving data in the same format as of javascript objects
-
-// an object
-let s1 = {
-    name: "Sumeet",
-    age: 34
-};
-// Note: s1 is an object. name and age are properties these are a.k.a data member.
-
-let s2 = {
-    name: "Moksh",
-    age: 25
-}
-
-// this is first way to create a array of objects
-let students = [s1, s2];
-console.log(s1.name);
-console.log(s1.age);
-
-// make it JSON
-let json = JSON.stringify((students));
-fs.writeFileSync(args.dest, json, "utf-8");
-
-let arrOfObjects = [
+let teams = [
     {
-        name: "Sumeet",
-        age: 34
+        name: "India",
+        rank: 1,
+        matches: [
+            {
+                vs: "England",
+                result: "win"
+            },
+            {
+                vs: "Australia",
+                result: "win"
+            }
+        ]
     },
-
     {
-        name: "Moksh",
-        age: 25
+        name: "Australia",
+        rank: 3,
+        matches: [
+            {
+                vs: "England",
+                result: "win"
+            },
+            {
+                vs: "India",
+                result: "lost"
+            }
+        ]
     },
-
     {
-        name: "Jasbir",
-        age: 24
+        name: "England",
+        rank: 2,
+        matches: [
+            {
+                vs: "India",
+                result: "lose"
+            },
+            {
+                vs: "Australia",
+                result: "lose"
+            }
+        ]
     }
-];
+]
 
-let newjson = JSON.stringify(arrOfObjects);
-fs.appendFileSync(args.dest, newjson, "utf-8");
-
-console.log(arrOfObjects[0].name);
-console.log(arrOfObjects[0].age);
-
-console.log(arrOfObjects[1].name);
-console.log(arrOfObjects[1].age);
-
-console.log(arrOfObjects[2].name);
-console.log(arrOfObjects[2].age);
+let json = JSON.stringify(teams);
+fs.writeFileSync(args.dest, json, "utf-8");
