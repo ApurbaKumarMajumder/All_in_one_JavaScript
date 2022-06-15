3de3a4e3f21208eeffae0ea29b5cf5f6bc8cacb5
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-06-16 00:50:01 +0530

    strating with cricinfo extractor project

---
+++ JavaScript/Projects/1_CricinfoExtracter.js
@@ -0,0 +1,17 @@
+// the urpose of this roject is to extract information of worldcup 2019 from cricinfo and  process
+// that in the form of excel and pdf scorecards
+// the real purpose is to learn how to extract information and get experience with js
+// A very good reason to ever make a project is to have good function
+
+// npm init -y
+// npm install minimist
+// npm install axios
+// npm install jsdom
+// npm install excel4node
+// npm install pdf-lib
+
+// download using axios
+// read using jsdom
+// make excel using excel4node
+// make pdf using pdf-lib
+
