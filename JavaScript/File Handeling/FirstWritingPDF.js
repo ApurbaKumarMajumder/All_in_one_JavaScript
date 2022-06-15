1c9a0e19e98e42c8348094729cd1ed29e90f37d1
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-06-15 09:17:02 +0530

    starting in a new computer

--- JavaScript/File Handeling/FirstWritingPDF.js
+++ JavaScript/File Handeling/FirstWritingPDF.js
@@ -1,5 +1,5 @@
 // npm install pdf-lib
-// node FirstWritingPDF.js --source=teams.json --dest=root
+// node FirstWritingPDF.js --source=teams.json --dest=worldcup
 
 const minimist = require("minimist");
 const fs = require("fs");
@@ -14,11 +14,21 @@ let teams = JSON.parse(teamsJSON);
 
 fs.mkdirSync(args.dest);
 for(let i=0; i<teams.length; i++){
-    let folderName = path.join(args.dest, teams[i].name);
-    fs.mkdirSync(folderName);
+    let teamFN = path.join(args.dest, teams[i].name);
+    fs.mkdirSync(teamFN);
     for(let j=0; j<teams[i].matches.length; j++){
-        let fileName = path.join(folderName, teams[i].matches[j].vs + ".pdf");
+        let matchFileName = path.join(teamFN, teams[i].matches[j].vs + ".pdf");
         // console.log(fileName);
-        fs.writeFileSync(fileName, "", "utf-8");
+        createScoreCard(teams[i].name, teams[i].matches[j], matchFileName);
     }
+}
+
+function createScoreCard(teamName, match, matchFileName){
+    // this fun creates pdf for match in appropriate folder with correct details
+    // here we will use pdf-lib to create the pdf
+    console.log(teamName);
+    console.log(match.vs);
+    console.log(match.result);
+    console.log(matchFileName);
+    console.log("----------------------------");
 }
