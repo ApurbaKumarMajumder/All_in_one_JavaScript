5a5c5700d6e090b52141c543ffa80bddc8c91e86
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-06-16 16:02:27 +0530

    more on cricinfo website

--- JavaScript/Projects/1_CricinfoExtracter.js
+++ JavaScript/Projects/1_CricinfoExtracter.js
@@ -10,8 +10,47 @@
 // npm install excel4node
 // npm install pdf-lib
 
+// node .\1_CricinfoExtracter.js --excel=Worldcup.csv --dataFolder=data --source=https://www.espncricinfo.com/series/icc-cricket-world-cup-2019-1144415/match-results
+
+let minimist = require("minimist");
+let axios = require("axios");
+let jsdom = require("jsdom");
+let excel4node = require("excel4node");
+let pdf = require("pdf-lib");
+
+let args = minimist(process.argv);
+
 // download using axios
 // read using jsdom
 // make excel using excel4node
 // make pdf using pdf-lib
 
+let responseKaPromise = axios.get(args.source);
+responseKaPromise.then(function(responce){
+    let html = responce.data;
+
+    let dom = new jsdom.JSDOM(html);
+    let document = dom.window.document;
+
+    let matches = [];
+    let matchScoreDivs = document.querySelectorAll("div.match-score-block");
+    for(let i=0; i<matchScoreDivs.length; i++){
+        let match = {
+
+        };
+
+        let namePs = matchScoreDivs[i].querySelectorAll("p.name");
+        match.t1 = namePs[0].textContent;
+        match.t2 = namePs[1].textContent;
+
+        let scoreSpans = matchScoreDivs[i].querySelectorAll("span.score");
+        match.t1s = "";
+        match.t2s = "";
+        match.result = "";
+        matches.push(match);
+    }
+    // let title = document.title;
+    // console.log(title);
+}).catch(function(err){
+    console.log(err);
+})
