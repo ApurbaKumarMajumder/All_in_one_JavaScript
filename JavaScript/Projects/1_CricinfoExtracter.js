024dc6d289df51a171fcdbdcd1442d9dbc15c295
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-06-19 19:30:10 +0530

    practicing the cricinfo project

--- JavaScript/Projects/1_CricinfoExtracter.js
+++ JavaScript/Projects/1_CricinfoExtracter.js
@@ -19,38 +19,115 @@ let excel4node = require("excel4node");
 let pdf = require("pdf-lib");
 
 let args = minimist(process.argv);
+// console.log(args.excel);
+// console.log(args.dataFolder);
+// console.log(args.source);
 
 // download using axios
-// read using jsdom
-// make excel using excel4node
-// make pdf using pdf-lib
+// extract information using jsdom
+// manipulate data using array functions
+// save in excel using excel4node
+// create folders and prepare pdfs
 
 let responseKaPromise = axios.get(args.source);
 responseKaPromise.then(function(responce){
     let html = responce.data;
+    // console.log(html);
 
     let dom = new jsdom.JSDOM(html);
     let document = dom.window.document;
 
     let matches = [];
-    let matchScoreDivs = document.querySelectorAll("div.match-score-block");
-    for(let i=0; i<matchScoreDivs.length; i++){
-        let match = {
+    // let match = {
 
+    // };
+    let matchDivs = document.querySelectorAll("div.match-score-block");
+    // console.log(matchDivs.length);
+
+    for(let i=0; i<matchDivs.length; i++){
+        let matchdiv = matchDivs[i];
+        let match = {
+            t1: "",
+            t2: "",
+            t1s: "",
+            t2s: "",
+            result: ""
         };
 
-        let namePs = matchScoreDivs[i].querySelectorAll("p.name");
-        match.t1 = namePs[0].textContent;
-        match.t2 = namePs[1].textContent;
+        let teamParas = matchdiv.querySelectorAll("div.name-detail > p.name");
+        match.t1 = teamParas[0].textContent;
+        match.t2 = teamParas[1].textContent;
 
-        let scoreSpans = matchScoreDivs[i].querySelectorAll("span.score");
-        match.t1s = "";
-        match.t2s = "";
-        match.result = "";
+        let scoreSpans = matchdiv.querySelectorAll("div.score_detail > span.score");
+        if(scoreSpans.length == 2){
+            match.t1s = scoreSpans[0].textContent;
+            match.t2s = scoreSpans[1].textContent;
+        } else if(scoreSpans.length == 1){
+            match.t1s = scoreSpans[0].textContent;
+            match.t2s = "";
+        } else {
+            match.t1s = "";
+            match.t2s = "";
+        }
+
+        let resultSpan = matchdiv.querySelector("div.status-text > span");
+        match.result = resultSpan.textContent;
+
         matches.push(match);
     }
-    // let title = document.title;
-    // console.log(title);
-}).catch(function(err){
-    console.log(err);
+
+    console.log(matches);
+    let teams = [];
+    for(let i=0; i<matches.length; i++){
+        populateTeams(teams, match);
+    }
 })
+
+function populateTeams(teams, match){
+    let t1idx = teams.findIndex(function(team){
+        
+    })
+}
+
+function populateMatchesInTeams(matches, teams, match){
+
+}
+//     let dom = new jsdom.JSDOM(html);
+//     let document = dom.window.document;
+
+//     let matches = [];
+//     let matchScoreDivs = document.querySelectorAll("div.match-score-block");
+//     for(let i=0; i<matchScoreDivs.length; i++){
+//         let match = {
+
+//         };
+
+//         let namePs = matchScoreDivs[i].querySelectorAll("p.name");
+//         match.t1 = namePs[0].textContent;
+//         match.t2 = namePs[1].textContent;
+
+//         let scoreSpans = matchScoreDivs[i].querySelectorAll("div.status-text > span.score");
+//         if (scoreSpans.length == 2) {
+//             match.t1s = scoreSpans[0].textContent;
+//             match.t2s = scoreSpans[1].textContent;  
+//         } else if (scoreSpans.length == 1) {
+//             match.t1s = scoreSpans[0].textContent;
+//             match.t2s = "";
+//         } else {
+//             match.t1s = "";
+//             match.t2s = "";
+//         }
+//         // match.result = "";
+
+//         let spanResult = matchScoreDivs[i].querySelector("div.status-text > span");
+//         match.result = spanResult.textContent;
+
+//         matches.push(match);
+//         console.log(i);
+//     }
+//     console.log(matches);
+//     // let title = document.title;
+//     // console.log(title);
+// }).catch(function(err){
+//     console.log(err);
+// })
