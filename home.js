be6822d73d8f7dace751a1387ccca205b89de85b
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-05-16 00:58:28 +0530

    starting with javascript

---
+++ home.js
@@ -0,0 +1,23 @@
+console.log("Hello");
+
+// alert("Hello this is Apurba");
+
+// How to write a comment inline
+
+// Variables
+var b = "smoothie";
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+// Manipulate DOM with Javascript
+/* ... It's just a Fancy way of saying
+change HTML with Javascript */
+
+// var age = prompt("What is your Age?");
+// document.getElementById("someText").innerHTML = age;
+
+// Number in Javascript
+var num1 = 5.7;
+console.log(5 * 10);
