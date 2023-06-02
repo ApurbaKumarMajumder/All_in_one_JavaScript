6f583838a33a6d09762dc21bb752b80c4f4bdd5c
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-06-02 23:11:49 +0530

    data types

---
+++ dataTypes.js
@@ -0,0 +1,7 @@
+let yourAge = 18; // number
+let yourName = "Bob" // string
+let name = {first: 'Jane', last: 'Deo'}; // object
+let truth = false; // boolean
+let groceries = ['apple', 'banana', 'oranges']; // array
+let random; // undefined
+let nothing = null; // value null
