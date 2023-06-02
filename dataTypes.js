b50b281fd706106b44391d7cfb5ebf3f111db7a1
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-06-02 23:29:04 +0530

    String in JavaScript

--- dataTypes.js
+++ dataTypes.js
@@ -5,3 +5,20 @@ let truth = false; // boolean
 let groceries = ['apple', 'banana', 'oranges']; // array
 let random; // undefined
 let nothing = null; // value null
+
+// String in JavaScript (common methods)
+let fruits = 'banana';
+let moreFruits = 'banana \napple';                          // new line
+
+console.log(moreFruits);
+console.log(fruits.length);
+console.log(fruits.indexOf('nan'));
+console.log(fruits.slice(2, 6));
+console.log(fruits.replace('ban', '123'));
+console.log(fruits.toUpperCase());
+console.log(fruits.toLowerCase());
+console.log(fruits[2]);
+console.log(fruits.split(''));   // split by characters
+
+fruits = 'banana, apple, orange, blackberry'
+console.log(fruits.split(','));  // split by a comma
