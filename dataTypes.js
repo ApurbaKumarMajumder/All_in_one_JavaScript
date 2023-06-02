634841af930d6d2e79b9b0a27b0aeec9340e4b1f
ApurbaKumarMajumder <9851apurba@gmail.com>
2023-06-02 23:54:52 +0530

    Array in javascript

--- dataTypes.js
+++ dataTypes.js
@@ -6,7 +6,7 @@ let groceries = ['apple', 'banana', 'oranges']; // array
 let random; // undefined
 let nothing = null; // value null
 
-// String in JavaScript (common methods)
+// 1. String in JavaScript (common methods)
 let fruits = 'banana';
 let moreFruits = 'banana \napple';                          // new line
 
@@ -22,3 +22,32 @@ console.log(fruits.split(''));   // split by characters
 
 fruits = 'banana, apple, orange, blackberry'
 console.log(fruits.split(','));  // split by a comma
+
+// 2. Array
+let fruit = ['banana', 'apple', 'orange', 'pineapples'];
+fruit = new Array('banana', 'apple', 'orange', 'pineapples');
+
+// alert(fruit[1]);
+console.log(fruit[2]);   // access value at index 2nd
+
+fruit[0] = 'pear';
+console.log(fruit);
+
+for (let i = 0; i < fruit.length; i++) {
+    console.log(fruit[i]);
+}
+
+// array common methods
+console.log('to String',fruit.toString());
+console.log(fruit.join(' * '));
+console.log(fruit);
+console.log(fruit.pop(), fruit);  // removes last item
+console.log(fruit.push('blackberries'), fruit);   // appends at last
+console.log(fruit[4]);
+// fruit[4] = 'new fruit';
+fruit[fruit.length] = 'new fruit';  // same as push
+console.log(fruit);
+fruit.shift();  // removes first element from an array
+console.log(fruit);
+fruit.unshift('kiwi');  // add first element to an array
+console.log(fruit);
