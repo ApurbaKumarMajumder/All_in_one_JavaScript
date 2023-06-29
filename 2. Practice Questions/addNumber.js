b9df7ee40d4927c21b0b32ddebe5578db085f93a
Apurba Kumar Majumder <9851apurba@gmail.com>
2023-06-29 20:51:45 +0530

    convert your age into seconds function

--- 2. Practice Questions/addNumber.js
+++ 2. Practice Questions/addNumber.js
@@ -4,6 +4,21 @@ function addNumbers(num1, num2) {
     return num1 + num2;
 }
 
-console.log(addNumbers(2, 5));
-console.log(addNumbers(5, 5));
-console.log(15, 5);
+// console.log(addNumbers(2, 5));
+// console.log(addNumbers(5, 5));
+// console.log(addNumbers(15, 5));
+
+function convertMinutesToSeconds(minutes) {
+    return minutes * 60;
+}
+
+// console.log(convertMinutesToSeconds(1));
+// console.log(convertMinutesToSeconds(2));
+
+function yourAgeInSeconds(age) {
+    return age * 12 * 30 * 24 * 60 * 60; 
+}
+
+console.log(yourAgeInSeconds(25));
+
+38
